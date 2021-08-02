b3cc297837ef5e6b032ec83620d2452ec6ff6008
Omake <kirusha1991@gmail.com>
2021-08-02 16:59:43 +0500

    Serializers finished

--- authapp/serializers.py
+++ authapp/serializers.py
@@ -2,10 +2,13 @@ from django.contrib.auth.models import User
 from rest_framework.serializers import ModelSerializer, CharField, RegexField
 
 
-class ReadOnlyUserSerializer(ModelSerializer):
+class AbstractUserSerializer(ModelSerializer):
     username = RegexField('^[\w.@+-]+$', max_length=150, min_length=1, allow_blank=False)
     first_name = CharField(max_length=30)
     last_name = CharField(max_length=150)
+
+
+class ReadOnlyUserSerializer(AbstractUserSerializer, ModelSerializer):
 
     class Meta:
         model = User
@@ -13,10 +16,8 @@ class ReadOnlyUserSerializer(ModelSerializer):
         read_only_fields = ['id', 'last_login', 'is_superuser']
 
 
-class WriteOnlyUserSerializer(ModelSerializer):
-    username = RegexField('^[\w.@+-]+$', max_length=150, min_length=1, allow_blank=False)
-    first_name = CharField(max_length=30)
-    last_name = CharField(max_length=150)
+class WriteOnlyUserSerializer(AbstractUserSerializer, ModelSerializer):
+    password = RegexField('^(?=.*[A-Z])(?=.*\d).{8,}$', max_length=128, min_length=1)
 
     class Meta:
         model = User
