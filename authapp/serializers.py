d7240e4583a906007643189a17a31031d7ca0ca0
Omake <kirusha1991@gmail.com>
2021-08-02 20:02:55 +0500

    views CRUD finished

--- authapp/serializers.py
+++ authapp/serializers.py
@@ -1,14 +1,16 @@
 from django.contrib.auth.models import User
 from rest_framework.serializers import ModelSerializer, CharField, RegexField
+from rest_framework.validators import UniqueValidator
 
 
 class AbstractUserSerializer(ModelSerializer):
-    username = RegexField('^[\w.@+-]+$', max_length=150, min_length=1, allow_blank=False)
+    username = RegexField('^[\w.@+-]+$', max_length=150, min_length=1, allow_blank=False,
+                          validators=[UniqueValidator(queryset=User.objects.all())])
     first_name = CharField(max_length=30)
     last_name = CharField(max_length=150)
 
 
-class ReadOnlyUserSerializer(AbstractUserSerializer, ModelSerializer):
+class ReadOnlyUserSerializer(AbstractUserSerializer):
 
     class Meta:
         model = User
@@ -16,7 +18,7 @@ class ReadOnlyUserSerializer(AbstractUserSerializer, ModelSerializer):
         read_only_fields = ['id', 'last_login', 'is_superuser']
 
 
-class WriteOnlyUserSerializer(AbstractUserSerializer, ModelSerializer):
+class WriteOnlyUserSerializer(AbstractUserSerializer):
     password = RegexField('^(?=.*[A-Z])(?=.*\d).{8,}$', max_length=128, min_length=1)
 
     class Meta:
--- authapp/views.py
+++ authapp/views.py
@@ -1,21 +1,71 @@
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
+from rest_framework import status
+
+from authapp.serializers import ReadOnlyUserSerializer, WriteOnlyUserSerializer
 
 
 class UserApiView(APIView):
     authentication_classes = [TokenAuthentication]
     permission_class = [AllowAny]
 
-    def get(self, request):
+    def get(self, request, pk=False):
         """Returns a list of users"""
-        users = User.objects.all()
+        if pk:
+            user = get_object_or_404(User, pk=pk)
+            serializer = ReadOnlyUserSerializer(user)
 
-        return Response(users)
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def post(self):
-        pass
+        else:
+            users = User.objects.all()
+            serializer = ReadOnlyUserSerializer(users, many=True)
 
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
+    def post(self, request):
+        """Create user"""
+        write_serializer = WriteOnlyUserSerializer(data=request.data)
+        if write_serializer.is_valid():
+            created_user = write_serializer.save()
+            read_serializer = ReadOnlyUserSerializer(created_user)
+
+            return Response(read_serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(write_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk):
+        """Update user"""
+        user = get_object_or_404(User, pk=pk)
+        write_serializer = WriteOnlyUserSerializer(user, data=request.data)
+        read_serializer = ReadOnlyUserSerializer(user)
+        if write_serializer.is_valid():
+            write_serializer.save()
+
+            return Response(read_serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(write_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk):
+        """Update user"""
+        user = get_object_or_404(User, pk=pk)
+        write_serializer = WriteOnlyUserSerializer(user, data=request.data)
+        read_serializer = ReadOnlyUserSerializer(user)
+        if write_serializer.is_valid():
+            write_serializer.save()
+
+            return Response(read_serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(write_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete (self, request, pk):
+        """Deactivate user"""
+        user = get_object_or_404(User, pk=pk)
+        user.is_active = False
+        user.save()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
